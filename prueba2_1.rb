d45f1e74911f668202e2fdbabb6dd237dbe099a8
gmocaceres <guillecaceres1989@gmail.com>
2019-05-14 15:30:59 -0400

    entrega final, menu y cada función probada en terminal

--- prueba2_1.rb
+++ prueba2_1.rb
@@ -1,105 +1,66 @@
 def read_alum(file_name)
-
     file = File.open(file_name, 'r')
-  
     alum = file.readlines.map(&:chomp).map { |lines| lines.split(', ') }
-  
     file.close
-  
     alum
- 
 end
   
 
-  def promedio
-  
+def promedio
     aux_arr = {}
-  
     notas = read_alum('alumnos.csv')
-  
-    notas.each do |item_line|
-  
-      acc = 0
-      cont = 0
-      
-      item_line.each_with_index do |elem, index|
-  
-      acc += elem.to_i / if index != 0
-      cont += 1 / if index != 0
-
-     end
-
-      puts "El alumno #{item_line[0]} promedio nota: #{acc/cont}"
-  
-      aux_arr[item_line[0]] = acc
-  
-    end
-  
-    return aux_arr
-  
-  end
-  
-
-  def inasistencias
-  
-    aux_arr = {}
-  
-    notas = read_alum('alumnos.csv')
-  
-    notas.each do |item_line|
-  
-      cont = 0
-      
-      item_line.each_with_index do |elem, index|
-  
-      cont += 1 / if elem != A
-
-     end
-
-      puts "alumno #{item_line[0]}, #{cont} inasistencias"
-  
-      aux_arr[item_line[0]] = acc
-  
-    end
-  
-    return aux_arr
-  
-  end
-  
-  def aprobación
-  
-    aux_arr = {}
-  
-    notas = read_alum('alumnos.csv')
-  
-    notas.each do |item_line|
-  
-      acc = 0
-      cont = 0
-      
-      item_line.each_with_index do |elem, index|
-  
-      acc += elem.to_i / if index != 0
-      cont += 1 / if index != 0
-
-     end
-
-    if #{acc/cont} >=5 
-
-      puts "El alumno #{item_line[0]} aprobó"
+    notas.each do |value_line|
+    acc = 0
+    cont = 0
+    value_line.each_with_index do |elem, index|
+    acc += elem.to_i if index != 0
     
-    else 
-
-      puts "El alumno #{item-line[0]} reprobó"
-     
-    end 
-  
-      aux_arr[item_line[0]] = acc
-  
+    cont += 1 if index != 0
+    
     end
-  
+    puts "Alumno #{value_line[0]} promedio nota: #{acc/cont}"
+    aux_arr[value_line[0]] = acc
+    end
     return aux_arr
+end
   
+
+def inasistencias
+  aux_arr = {}
+  notas = read_alum('alumnos.csv')
+  notas.each do |value_line|
+  cont = 0
+  value_line.each_with_index do |elem, index|
+  cont += 1 if elem != "A"
+  end
+  puts "Alumno #{value_line[0]}, #{cont} inasistencias"
+  aux_arr[value_line[0]] = cont
+  end
+  return aux_arr
+  
+end
+  
+def aprobación
+  aux_arr = {}
+  notas = read_alum('alumnos.csv')
+  notas.each do |value_line|
+  acc = 0
+  cont = 0
+  value_line.each_with_index do |elem, index|
+  acc += elem.to_i if index != 0
+  
+  cont += 1 if index != 0
+
+  end
+
+  if acc/cont >=5 
+  puts "El alumno #{value_line[0]} aprobó"
+  else 
+  puts "El alumno #{value_line[0]} reprobó"
+  end 
+  aux_arr [value_line[0]] = acc
+  end
+  return aux_arr
   end
   
   
@@ -133,9 +94,8 @@ end
   
     else
   
-      puts "Opción no válida"
+      puts "no valido: 1 = promedio cada alumno. 2 = inasistencias cada alumno.
+      3 = aprobación cada alumno. 4 = salir"
   
     end
-  
-  end
-  
+end
