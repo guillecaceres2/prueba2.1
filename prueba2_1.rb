3a5fb41788fa64d73ef3218265217396fad66019
gmocaceres <guillecaceres1989@gmail.com>
2019-05-14 13:59:33 -0400

    se añade archivo de notas de alumnos y version 1 de visualizador de estadisticas del curso

---
+++ prueba2_1.rb
@@ -0,0 +1,141 @@
+def read_alum(file_name)
+
+    file = File.open(file_name, 'r')
+  
+    alum = file.readlines.map(&:chomp).map { |lines| lines.split(', ') }
+  
+    file.close
+  
+    alum
+ 
+end
+  
+
+  def promedio
+  
+    aux_arr = {}
+  
+    notas = read_alum('alumnos.csv')
+  
+    notas.each do |item_line|
+  
+      acc = 0
+      cont = 0
+      
+      item_line.each_with_index do |elem, index|
+  
+      acc += elem.to_i / if index != 0
+      cont += 1 / if index != 0
+
+     end
+
+      puts "El alumno #{item_line[0]} promedio nota: #{acc/cont}"
+  
+      aux_arr[item_line[0]] = acc
+  
+    end
+  
+    return aux_arr
+  
+  end
+  
+
+  def inasistencias
+  
+    aux_arr = {}
+  
+    notas = read_alum('alumnos.csv')
+  
+    notas.each do |item_line|
+  
+      cont = 0
+      
+      item_line.each_with_index do |elem, index|
+  
+      cont += 1 / if elem != A
+
+     end
+
+      puts "alumno #{item_line[0]}, #{cont} inasistencias"
+  
+      aux_arr[item_line[0]] = acc
+  
+    end
+  
+    return aux_arr
+  
+  end
+  
+  def aprobación
+  
+    aux_arr = {}
+  
+    notas = read_alum('alumnos.csv')
+  
+    notas.each do |item_line|
+  
+      acc = 0
+      cont = 0
+      
+      item_line.each_with_index do |elem, index|
+  
+      acc += elem.to_i / if index != 0
+      cont += 1 / if index != 0
+
+     end
+
+    if #{acc/cont} >=5 
+
+      puts "El alumno #{item_line[0]} aprobó"
+    
+    else 
+
+      puts "El alumno #{item-line[0]} reprobó"
+     
+    end 
+  
+      aux_arr[item_line[0]] = acc
+  
+    end
+  
+    return aux_arr
+  
+  end
+  
+  
+
+  condition = 0
+  
+  while condition != 4
+  
+    puts "Elije una opción, 1 = promedio cada alumno. 2 = inasistencias cada alumno.
+     3 = aprobación cada alumno. 4 = salir"
+  
+    condition = gets.chomp.to_i
+  
+    case condition
+  
+    when 1
+  
+      promedio
+  
+    when 2
+  
+     inasistencias
+  
+    when 3
+  
+      aprobación
+  
+    when 4
+  
+      puts "salida"
+  
+    else
+  
+      puts "Opción no válida"
+  
+    end
+  
+  end
+  
